9adb752d06c9ebea22221711ac17c2affa8a9196
ibsy-dev <38604522+ibsy-dev@users.noreply.github.com>
2018-04-22 18:37:46 +0800

    Create trending-twitter1.py
    verify auth

---
+++ trending-twitter1.py
@@ -0,0 +1,11 @@
+pip install python twitter
+
+import twitter
+
+api = twitter.Api(consumer_key="HdNFQ0YH52wyrwtUYiwalIp3b",
+consumer_secret="dC6Uc13Xhrtsel92uBlD3r6QyEkWq8oCFR6psW8rCsURR4YQaY",
+access_token_key="46344285-UOV7LAxBlelMHA59eYH1wteT4FssMjoBQJJIq1UPU",
+access_token_secret="FXSMZSOZameYmsfbP2Gac2COGPPF8ZDJWoWyQniLxVQxz")
+
+print(api.VerifyCredentials())
+
